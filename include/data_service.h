05e3df20af9aee3140c07870483f89c698f930df
Phobosmir <phobos_mir@mail.ru>
2016-07-21 21:47:12 +0000

    Added correct responses

--- include/data_service.h
+++ include/data_service.h
@@ -1,3 +1,6 @@
 
 int process_request(int fd);
-void parse_request(char *request);
+void parse_request(char *request, char *uri);
+int is_file_exists(char* fname);
+void send_not_found(int fd);
+void send_data(int fd, char *filename);
